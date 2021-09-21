07a667aa98662aff39a4d8bd7732b21a15e574ef
Iman <iman_iho@yahoo.com>
2021-09-21 18:43:32 +0430

    responsive register page

--- src/components/pages/register.js
+++ src/components/pages/register.js
@@ -11,21 +11,41 @@ export const Register = () => (
         <div>
             <Header />
             <div className='container mb-5'> 
-                <MainTitle 
-                    title= 'همین الان عضو شوید'
-                    size= '2.5em'
-                    color= 'success'
-                />
-                <CenterDivider 
-                    width= '60px'
-                    color= 'gray'
-                />
-                <div className='row mt-5' style={{background: '#FAFAFA'}}>
-                    <div className='col'>
-                        <img src='img/register.jpg' style={{width:'600px', height:'600px'}} alt='' />
-                    </div>
-                    <div className='col'>
-                        <RegisterForm />
+                <div className='row'>
+                    <div className='col-xl-10 offset-xl-1'>
+                        <MainTitle 
+                            title= 'همین الان عضو شوید'
+                            size= '2.5em'
+                            color= 'success'
+                        />
+                        <CenterDivider 
+                            width= '60px'
+                            color= 'gray'
+                        />
+                        <div className='row mt-5' 
+                             style={{
+                                 background: '#FAFAFA',
+                                 borderRadius:'7px 0px 0px 7px'
+                                }}
+                        >
+                            <div className='col-xl-5'>
+                                <img 
+                                    className='register-image'
+                                    src='img/register.jpg' 
+                                    style={{
+                                        width:'500px',
+                                         height:'600px',
+                                         borderRadius:'0px 7px 7px 0px'
+                                        }} 
+                                    alt='' 
+                                />
+                            </div>
+                            <div 
+                                className='col-xl-6 offset-xl-1'
+                            >
+                                <RegisterForm />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
